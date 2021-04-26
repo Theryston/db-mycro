7ed2b738479b37a2f3bdc2898d8349396550269b
Theryston <funktodo2@gmail.com>
2021-04-26 07:24:46 -0300

    method save

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -3,10 +3,11 @@ import { Database } from '../database/Database'
 
 interface IColumn {
   name: string;
-  type: 'string' | 'number' | 'array' | 'object';
+  type: 'string' | 'number' | 'array' | 'object' | 'boolean';
   isPrimary ? : boolean;
   autoIncrement ? : boolean;
   default ? : any;
+  allowNull ? : boolean;
 }
 
 interface IRepository {
@@ -20,23 +21,28 @@ export class Repository {
   public db: Database;
   public path: string;
   public columns: IColumn[];
-  public datas?: object[];
+  public datas ? : object[];
 
   constructor({ name, columns, db }: IRepository) {
     this.name = name;
     this.columns = columns;
     this.db = db;
-    this.datas = [{}]
     this.path = this.db.path + '/' + this.name + '.json'
-    this.save()
+    this.initDatas()
   }
 
-  public save() {
+  private initDatas() {
     if (fs.existsSync(this.path)) {
       this.datas = JSON.parse(fs.readFileSync(this.path, "utf8"))
     } else {
       const datas = JSON.stringify(this.datas)
       fs.writeFileSync(this.path, datas)
     }
+  }
+
+
+  public save() {
+    const datas = JSON.stringify(this.datas)
+    fs.writeFileSync(this.path, datas)
   }
 }
